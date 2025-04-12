d33df0a842fcc52e178c93512eba8fb9c186520b
sabina-filousova <s.filousova@gmail.com>
2025-04-12 13:49:22 +0200

    Upraveno

--- HW01.py
+++ HW01.py
@@ -1,18 +1,17 @@
+import json  
+
 with open("alice.txt", mode = "r", encoding = "utf-8") as soubor:
     text = soubor.read()
-    text = text.lower()
-    text = text.replace(' ', "")
-    text = text.replace('\n', "")
+    text = text.lower().replace("" "", "").replace("\n", "")
 
     slovnik = {}
     for znak in text:
         if znak not in slovnik:
             slovnik[znak] = 1
         else:
-            slovnik[znak] = slovnik[znak] + 1
-
+            slovnik[znak] = slovnik[znak] + 1 #jakože vím, že profi vypadá +=, ale pro mě je to jako pro mozek špatně pochopitelné, a pak nechápu svůj vlastní kód :D 
+            #slovnik[znak] += 1
 serazene = dict(sorted(slovnik.items()))
 
-import json  
 with open("hw01_output.json", mode = "w", encoding = "utf-8") as soubor2:
-    json.dump(serazene, soubor2, indent = 4, ensure_ascii=False)
+    json.dump(serazene, soubor2, indent = 4, ensure_ascii=False, sort_keys=True)
