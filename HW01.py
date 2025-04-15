bd84b669f3faa0a5025f301dcb53eb10edf50287
sabina-filousova <s.filousova@gmail.com>
2025-04-15 18:05:59 +0200

    Konečná úprava

--- HW01.py
+++ HW01.py
@@ -1,6 +1,6 @@
 import json  
 
-with open("alice.txt", mode = "r", encoding = "utf-8") as soubor:
+with open("alice.txt", mode="r", encoding="utf-8") as soubor:
     text = soubor.read()
     text = text.lower().replace("" "", "").replace("\n", "")
 
@@ -10,8 +10,6 @@ with open("alice.txt", mode = "r", encoding = "utf-8") as soubor:
             slovnik[znak] = 1
         else:
             slovnik[znak] = slovnik[znak] + 1
-            #slovnik[znak] += 1 mám ale radši moje vypsání, lépe se v tom orientuji :)
-serazene = dict(sorted(slovnik.items()))
 
-with open("hw01_output.json", mode = "w", encoding = "utf-8") as soubor2:
-    json.dump(serazene, soubor2, indent = 4, ensure_ascii=False, sort_keys=True)
+with open("hw01_output.json", mode="w", encoding="utf-8") as soubor2:
+    json.dump(slovnik, soubor2, indent=4, ensure_ascii=False, sort_keys=True)
